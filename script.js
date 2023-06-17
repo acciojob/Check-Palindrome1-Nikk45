95616f16e30c4c4e6092cbf11217a27db580bf18
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-06-17 22:32:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-	const strr = str.split('').reverse().join('').toLowerCase();
-	// alert(str);
+	const strr = str.split('').reverse().join('');
+	alert(str.toLowerCase());
 	if(str === strr){ 
 		return true;
 	}
 	return false;
 }
 
-let res = palindrome("racecar")
+let res = palindrome("Racecar")
 console.log(res);
 
 module.exports = palindrome
