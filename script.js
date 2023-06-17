5811811cd8efb7a0ae6985e381b95fa6078b1295
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-06-17 21:38:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 function palindrome(str){
 	let strr = str.split(',').reverese().join("");
-	str === strr? console.log(true) : console.log(false);
+	str === strr? return true : return false;
 }
+
+palindrome(racecar);
+
 module.exports = palindrome
